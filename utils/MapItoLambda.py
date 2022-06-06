8821f54ee440e02b2d7f48234844324dd0c2817a
riverback <hhz_pku@163.com>
2022-06-06 20:08:49 +0800

    完成波长与rgb颜色的映射函数

--- utils/MapItoLambda.py
+++ utils/MapItoLambda.py
@@ -1,14 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from sympy import solve, sin, cos, Symbol, re
-
+import sympy
+from sympy import solve, sin, cos, Symbol, re, acos
 
 
 D = 1 / 1200 * 1e-3  # 光栅常数 1/1200 mm
-DELTA_THETA = 12.5 / 4096  # 单位为° 指的是i每+1，对应的角度变化
+DELTA_THETA = (12.5 / 4096) * sympy.pi / 180  # 指的是i每+1，对应的角度变化 单位是弧度
 
-
-def CirctoRad(theta_circ): return theta_circ / 180.0 * np.pi
+def CirctoRad(theta_circ): return theta_circ / 180.0 * sympy.pi
 
 
 def Calibrate(cal_i: np.ndarray, cal_lambda: np.ndarray):
@@ -17,44 +16,71 @@ def Calibrate(cal_i: np.ndarray, cal_lambda: np.ndarray):
         只有一个校准点，此时如果要正常工作，需要调节面镜初始角度使得在旋转的一侧极限处，接收到校准信号，因此理论上 cal_i 会接近0 或者接近4095
         关于面镜旋转方向，可能要在后续固定镜子后确定镜子旋转方向后再进行确定
         """
-        # lambda = 2 * d * sin(pi/2) * cos(phi - 7/72 * pi)
-        x = Symbol('x') # phi_0 - 7/72*pi
-        phi_0 = solve(cal_lambda - 2 * D * cos(x))  # 由于返回解的范围是0-2pi，因此必定会有两个解 phi_0 已经包含了 - 7/72*pi
-        print(phi_0)
-        print(phi_0[0])
-        print(phi_0[0][x])
+        # lambda = 2 * d * sin(theta + 7/72*pi) * cos(phi - 7/72 * pi)
+        x = Symbol('x')  # phi_0 - 7/72*pi
+        # 由于返回解的范围是0-2pi，因此必定会有两个解 theta 已经包含了 - 7/72*pi
+        cos_theta = solve(cal_lambda[0] - 2 * D * x * sin(cal_i[0] * DELTA_THETA + 7/72*sympy.pi))
+        
+        # print("cos_theta", cos_theta)
+
+
         def map_fun(i):
             # 这里实现的是默认 cal_i 非常接近0
-            return 2 * D * cos(CirctoRad(DELTA_THETA*(i-cal_i[0]))+re(phi_0[1][x]))
-        
+            # print(DELTA_THETA*(i-cal_i[0]))
+            if len(cos_theta) > 1:
+                return 2 * D * max(*cos_theta) * sin(i * DELTA_THETA + 7/72*sympy.pi)
+            else:
+                return 2 * D * cos_theta[0] * sin(i * DELTA_THETA + 7/72*sympy.pi)
+
         return map_fun
+    
     elif cal_i.shape == (2, ):
         """
-        有两个校准点，此时可以直接通过方程求解给出映射公式，但是仍需要根据镜子具体的旋转方向(即i变大时phi是变大还是变小)
+        有两个校准点 此时增加一个修正常数C 即 phi = i*DELTA_THETA + C
         """
-        x = Symbol('x') # phi_1 - 7/72*pi
-        y = Symbol('y') # phi_2 - 7/72*pi
-        z = Symbol('z') # sin(theta + 7/72 * pi)
-        roots = solve([cal_lambda[0] - 2*D*z*cos(x), cal_lambda[1] - 2*D*z*cos(y), y-x-CirctoRad((cal_i[1]-cal_i[0])*DELTA_THETA)], [x,y,z])
-        # 由于第三个方程是通过i的差值给出两次phi的插值，因此最终给出映射的时候就只使用phi_1了
-        print(f"roots:{roots}")
+        # lambda = 2 * d * sin(theta + 7/72*pi) * cos(phi - 7/72 * pi)
+        x = Symbol('x')  # phi_0 - 7/72*pi
+        # 由于返回解的范围是0-2pi，因此必定会有两个解 theta 已经包含了 - 7/72*pi
+        cos_theta = solve(cal_lambda[0] - 2 * D * x * sin(cal_i[0] * DELTA_THETA + 7/72*sympy.pi))
+        # print("cos_theta", cos_theta)
+
+        ### 然后求修正常数C
+        y = Symbol('y')
+        if len(cos_theta) > 1:
+            C = (solve(cal_lambda[0] - 2 * D * max(*cos_theta) * sin(cal_i[1] * DELTA_THETA + y + 7/72*sympy.pi)))
+        else:
+            C = (solve(cal_lambda[0] - 2 * D * cos_theta[0] * sin(cal_i[1] * DELTA_THETA + y + 7/72*sympy.pi)))
+            
+        # print("C", C)
+        
         def map_fun(i):
-            return 2 * D * cos(CirctoRad(DELTA_THETA*(i-cal_i[0])+roots[x][0]))
+            if len(cos_theta) > 1:
+                return 2 * D * max(*cos_theta) * sin(i * DELTA_THETA + C[0] + 7/72*sympy.pi)
+            else:
+                return 2 * D * cos_theta[0] * sin(i * DELTA_THETA + C[0] + 7/72*sympy.pi)
+        
+        return map_fun
 
 
 if __name__ == '__main__':
+    
+    print("\n\n\n### 一个校准点 ###")
+    
     cal_i1 = np.array([0, ])
-    cal_lambda1 = np.array([1228.0, ])
+    cal_lambda1 = np.array([423.0*1e-9, ])
     f1 = Calibrate(cal_i1, cal_lambda1)
-    print("hello")
-    print(f1(0))
-    for i in range(0, 4000, 300):
-        print(f"{i}: lambda: {f1(i)*1e9}nm")
-    '''
+    
+    print(f1(0).evalf()*1e9, '     ', f1(4095).evalf()*1e9, '     ', f1(4095).evalf()*1e9-f1(0).evalf()*1e9)
+    '''for i in range(0, 4000, 300):
+        print(f"{i}: lambda: {f1(i).evalf()*1e9} nm")'''
+    
+    print("### 两个校准点 ###\n\n\n")
+    
     cal_i2 = np.array([0, 30])
     cal_lambda2 = np.array([1105.0*1e-9, 1434.0*1e-9])
     f2 = Calibrate(cal_i2, cal_lambda2)
-    for i in range(0, 100, 10):
-        print(f"{i}: lambda{f2(i)}")
-    '''
+    print(f2(0).evalf()*1e9, '     ', f2(4095).evalf()*1e9, '     ', f2(4095).evalf()*1e9-f2(0).evalf()*1e9)
+    '''for i in range(0, 4000, 100):
+        print(f"{i}: lambda: {f2(i).evalf() * 1e9} nm")'''
+
     print("success?")
---
+++ utils/plot_and_save.py
@@ -0,0 +1,88 @@
+import os
+import qt5_applications
+dirname = os.path.dirname(qt5_applications.__file__)
+plugin_path = os.path.join(dirname, 'Qt', 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.interpolate import make_interp_spline
+
+
+def getRGB(dWave: float, maxPix=1, gamma=1):
+    """根据波长绘制颜色
+
+    Args:
+        dWave (float): 波长
+        maxPix (int, optional): 最大值. Defaults to 1.
+        gamma (int, optional): 调教参数. Defaults to 1.
+
+    Returns:
+        _type_: 波长对应颜色的rgb值
+    """
+    waveArea = [380,440,490,510,580,645,780]
+    minusWave = [0,440,440,510,510,645,780]
+    deltWave = [1,60,50,20,70,65,35]
+    for p in range(len(waveArea)):
+        if dWave<waveArea[p]:
+            break
+
+    pVar = abs(minusWave[p]-dWave)/deltWave[p]
+    rgbs = [[0,0,0],[pVar,0,1],[0,pVar,1],[0,1,pVar],
+            [pVar,1,0],[1,pVar,0],[1,0,0],[0,0,0]]
+    
+    #在光谱边缘处颜色变暗
+    if (dWave>=380) & (dWave<420):
+        alpha = 0.3+0.7*(dWave-380)/(420-380)
+    elif (dWave>=420) & (dWave<701):
+        alpha = 1.0
+    elif (dWave>=701) & (dWave<780):
+        alpha = 0.3+0.7*(780-dWave)/(780-700)
+    else:
+        alpha = 0       #非可见区
+
+    return [maxPix*(c*alpha)**gamma for c in rgbs[p]]
+
+
+def drawSpec():
+    pic = np.zeros([100, 360, 3])
+    rgb = [getRGB(d) for d in range(400, 760)]
+    pic = pic + rgb
+    plt.imshow(pic)
+    plt.yticks(range(0, 100, 10), [str(0 + 10 * i) for i in range(10)])
+    plt.xticks(range(0, 360, 50), [str(400+ 50 * i) for i in range(int((760-400)/50)+1)])
+    plt.savefig('Spec from 400 to 760')
+    plt.show()
+
+
+def smooth_xy(lx: np.ndarray, ly: np.ndarray):
+    """数据平滑处理
+
+    Args:
+        lx (np.ndarray): x轴数据
+        ly (np.ndarray): y轴数据
+    """
+
+    x_smooth = np.linspace(lx.min(), ly.min(), 400)
+    y_smooth = make_interp_spline(lx, ly)(x_smooth)
+
+    return (x_smooth, y_smooth)
+
+
+def line_chart(lx: np.ndarray, ly: np.ndarray):
+    """折线图
+
+    Args:
+        lx (np.ndarray): x轴数据
+        ly (np.ndarray): y轴数据
+    """
+    fig, ax = plt.subplots()
+    ax.plot()
+
+
+
+
+
+if __name__ == '__main__':
+    drawSpec()
